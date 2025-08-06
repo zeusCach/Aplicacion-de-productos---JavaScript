627ea4647cc28644a9ff90708ad3e2e50ee020d7
ZeusDeev <zeus.cach@outlook.com>
2025-08-06 00:16:46 -0500

    creando estructura de nuestra logica(App)

--- App.js
+++ App.js
@@ -1,1 +1,24 @@
 // Logica de la aplicacion
+class product {
+    constructor(name, price, year){
+        this.name = name
+        this.price = price
+        this.year = year
+    }
+}
+
+class UI {
+    addProduct() {
+
+    }
+
+    deletProduct() {
+
+    }
+
+    showMessage(){
+
+    }
+}
+
+//DOM Events
